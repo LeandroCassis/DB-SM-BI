eb71d21fbc4a32c796dbe03f75fb3db063018856
Leandro Assis <leandro.assis@vesperttine.com>
2026-04-02 04:54:35 -0300

    UPDATE

--- Views/dbo.WH_ALERTAS_FADIGA.sql
+++ Views/dbo.WH_ALERTAS_FADIGA.sql
@@ -1,43 +1,88 @@
 ﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
 CREATE VIEW [dbo].[WH_ALERTAS_FADIGA] 
-AS /*
-=====================================================================
- CONSULTA: ALERTAS DE FADIGA POR EMPRESA, EQUIPAMENTO E OPERADOR
- FUNCIONALIDADE: Análise detalhada de alertas de fadiga com hierarquia organizacional
- DATA DE CRIAÇÃO: 10/09/2025
- ÚLTIMA ATUALIZAÇÃO: 10/09/2025
- DESENVOLVIDO POR: VESPERTTINE (vesperttine.com)
- SISTEMA: Sistema RajaMine - Monitoramento de Fadiga SM METAIS
-=====================================================================
-*/
-
-SELECT 
-    UPPER(Empresa.Descricao) 'EMPRESA',                            -- Nome da empresa
-    UPPER(Operador.Nome) 'OPERADOR',                               -- Nome do operador
-    UPPER(Equipamento.Descricao) 'EQUIPAMENTO',                    -- Nome do equipamento
-    FORMAT(AlertaFadiga.Data, 'dd/MM/yyyy') 'DATA',                        -- Data do alerta
-    FORMAT(AlertaFadiga.Data, 'HH:mm') 'HORA',                     -- Hora do alerta
-    UPPER(Turno.Descricao) 'TURNO',                                -- Descrição do turno
-    TipoAlertaFadiga.Codigo 'COD ALERTA',                          -- Código do tipo de alerta
-    UPPER(TipoAlertaFadiga.Descricao) 'TIPO ALERTA',              -- Tipo de alerta de fadiga
-    AlertaFadiga.Velocidade '#VELOCIDADE',                         -- Velocidade registrada
-    CASE AlertaFadiga.Situacao
+AS SELECT
+UPPER(Empresa.Descricao) 'EMPRESA', 
+AlertaFadiga.Id 'ID ALERTA',
+AlertaFadiga.IdEquipamento 'ID EQUIPAMENTO',
+UPPER(Equipamento.Descricao) 'EQUIPAMENTO',
+AlertaFadiga.IdOperador 'ID OPERADOR',
+UPPER(_OP.Nome) 'OPERADOR',
+AlertaFadiga.IdTurno 'ID TURNO',
+UPPER(Turno.Descricao) 'TURNO', 
+AlertaFadiga.TipoAlerta 'COD TIPO ALERTA',
+UPPER(TipoAlertaFadiga.Descricao) 'TIPO ALERTA',
+CAST(AlertaFadiga.Data AS DATE) 'DATA ALERTA',
+AlertaFadiga.Data 'DATA HORA ALERTA',
+AlertaFadiga.Velocidade 'VELOCIDADE',
+AlertaFadiga.DataSync 'DATA INTEGRAÇÃO',
+AlertaFadiga.Situacao 'COD SITUAÇÃO',
+CASE AlertaFadiga.Situacao
         WHEN 'P' THEN 'PENDENTE'
         WHEN 'V' THEN 'VÁLIDA'
         ELSE 'INVÁLIDA'
-    END 'SITUAÇÃO'                                                 -- Status do alerta
-FROM RajaMine.dbo.AlertaFadiga WITH (NOLOCK)                      -- Tabela principal de alertas
-INNER JOIN RajaMine.dbo.TipoAlertaFadiga WITH (NOLOCK)            -- Tipos de alerta
-    ON TipoAlertaFadiga.Codigo = AlertaFadiga.TipoAlerta
-LEFT JOIN RajaMine.dbo.Turno WITH (NOLOCK)                        -- Turnos de trabalho
-    ON Turno.Id = AlertaFadiga.IdTurno
-LEFT JOIN RajaMine.dbo.Equipamento WITH (NOLOCK)                  -- Equipamentos monitorados
-    ON Equipamento.Id = AlertaFadiga.IdEquipamento
-LEFT JOIN RajaMine.dbo.Operador WITH (NOLOCK)                     -- Operadores dos equipamentos
-    ON Operador.Id = AlertaFadiga.IdOperador
-LEFT JOIN RajaMine.dbo.Empresa WITH (NOLOCK)                      -- Empresas do grupo
-    ON Equipamento.IdEmpresa = Empresa.Id
-WHERE AlertaFadiga.Data >= '2025-01-01'           -- Últimos 30 dias
---ORDER BY AlertaFadiga.Data DESC, Equipamento.Descricao           -- Ordenar por data e equipamento 
+        END 'SITUAÇÃO',
+AlertaFadiga.IdEquipamento 'ID USUÁRIO AVALIAÇÃO',
+UPPER(_UA.Nome) 'USUÁRIO AVALIAÇÃO',
+IIF(TipoAlertaFadiga.Risco='A', 'ALTO',
+IIF(TipoAlertaFadiga.Risco='M', 'MÉDIO',
+IIF(TipoAlertaFadiga.Risco='B', 'BAIXO','VERIFICAR'))) 'RISCO',
+AlertaFadiga.ClassificacaoAlerta 'COD CLASSIFICAÇÃO',
+IIF(AlertaFadiga.ClassificacaoAlerta='I', 'ISOLADO',
+IIF(AlertaFadiga.ClassificacaoAlerta='R', 'REINCIDENTE',
+IIF(AlertaFadiga.ClassificacaoAlerta='C', 'CRÍTICO','NÃO CLASSIFICADO'))) 'CLASSIFICAÇÃO'
+
+FROM RajaMine.dbo.AlertaFadiga WITH (NOLOCK)
+LEFT JOIN RajaMine.dbo.TipoAlertaFadiga  WITH (NOLOCK)
+ON AlertaFadiga.IdTipoAlertaFadiga = TipoAlertaFadiga.Id
+
+LEFT JOIN RajaMine.dbo.Equipamento  WITH (NOLOCK)
+ON AlertaFadiga.IdEquipamento = Equipamento.Id
+
+LEFT JOIN RajaMine.dbo.Usuario _UA  WITH (NOLOCK)
+ON AlertaFadiga.IdUsuarioAvaliacao = _UA.Id
+
+LEFT JOIN RajaMine.dbo.Operador _OP WITH (NOLOCK)
+ON AlertaFadiga.IdOperador = _OP.Id
+
+LEFT JOIN RajaMine.dbo.Empresa WITH (NOLOCK)                   
+ON Equipamento.IdEmpresa = Empresa.Id
+
+LEFT JOIN RajaMine.dbo.Turno WITH (NOLOCK)                     
+ON Turno.Id = AlertaFadiga.IdTurno
+
+
+WHERE AlertaFadiga.Data >= '2025-01-01' 
+
+
+
+
+--SELECT 
+--    UPPER(Empresa.Descricao) 'EMPRESA',                            -- Nome da empresa
+--    UPPER(Operador.Nome) 'OPERADOR',                               -- Nome do operador
+--    UPPER(Equipamento.Descricao) 'EQUIPAMENTO',                    -- Nome do equipamento
+--    FORMAT(AlertaFadiga.Data, 'dd/MM/yyyy') 'DATA',                        -- Data do alerta
+--    FORMAT(AlertaFadiga.Data, 'HH:mm') 'HORA',                     -- Hora do alerta
+--    UPPER(Turno.Descricao) 'TURNO',                                -- Descrição do turno
+--    TipoAlertaFadiga.Codigo 'COD ALERTA',                          -- Código do tipo de alerta
+--    UPPER(TipoAlertaFadiga.Descricao) 'TIPO ALERTA',              -- Tipo de alerta de fadiga
+--    AlertaFadiga.Velocidade '#VELOCIDADE',                         -- Velocidade registrada
+--    CASE AlertaFadiga.Situacao
+--        WHEN 'P' THEN 'PENDENTE'
+--        WHEN 'V' THEN 'VÁLIDA'
+--        ELSE 'INVÁLIDA'
+--    END 'SITUAÇÃO'                                                 -- Status do alerta
+--FROM RajaMine.dbo.AlertaFadiga WITH (NOLOCK)                      -- Tabela principal de alertas
+--INNER JOIN RajaMine.dbo.TipoAlertaFadiga WITH (NOLOCK)            -- Tipos de alerta
+--    ON TipoAlertaFadiga.Codigo = AlertaFadiga.TipoAlerta
+--LEFT JOIN RajaMine.dbo.Turno WITH (NOLOCK)                        -- Turnos de trabalho
+--    ON Turno.Id = AlertaFadiga.IdTurno
+--LEFT JOIN RajaMine.dbo.Equipamento WITH (NOLOCK)                  -- Equipamentos monitorados
+--    ON Equipamento.Id = AlertaFadiga.IdEquipamento
+--LEFT JOIN RajaMine.dbo.Operador WITH (NOLOCK)                     -- Operadores dos equipamentos
+--    ON Operador.Id = AlertaFadiga.IdOperador
+--LEFT JOIN RajaMine.dbo.Empresa WITH (NOLOCK)                      -- Empresas do grupo
+--    ON Equipamento.IdEmpresa = Empresa.Id
+--WHERE AlertaFadiga.Data >= '2025-01-01'           -- Últimos 30 dias
+
 GO
---
+++ Views/dbo.WH_QUALIDADE_POR_PROCESSO.sql
@@ -0,0 +1,243 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE  VIEW [dbo].[WH_QUALIDADE_POR_PROCESSO] AS
+
+WITH BaseQuery AS (
+SELECT --TOP 100
+ t.IdEmpresa 'COD EMPRESA'
+ ,UPPER(tp.descricao) 'TIPO PROCESSO'
+ ,cast(pr.Data as date)  'DATA'
+ ,pr.Massa 'TONELADAS'
+ ,pr.TempoProcessamento 'TEMPO PROCESSAMENTO'
+ ,UPPER(t.Descricao) 'TURNO'
+ ,ISNULL(pr.AnaliseQuimicaInformada, 'S') 'ANÁLISE INFORMADA'
+ ,ISNULL(pr.AnaliseGranulometricaInformada, 'S') 'ANÁLISE GRANOLOMÉTRICA INFORMADA'
+ ,pr.EL1
+ ,pr.EL2
+ ,pr.EL3
+ ,pr.EL4
+ ,pr.EL5
+ ,pr.EL6
+ ,pr.EL7
+ ,--QuÃ­mica
+  pr.EL8
+ ,pr.EL9
+ ,pr.EL10
+ ,pr.EL11
+ ,pr.EL12
+ ,pr.EL13
+ ,pr.EL14
+ ,pr.EL15
+ ,pr.EL16
+ ,pr.EL17
+ ,--GranulomÃ©trica
+  (SELECT
+      e.Sigla
+    FROM  RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL1'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl1
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL2'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl2
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL3'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl3
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL4'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl4
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL5'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl5
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL6'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl6
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL7'
+    AND e.Tipo = 0--QuÃ­mica
+  )
+  NomeEl7
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL8'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl8
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL9'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl9
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL10'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl10
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL11'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl11
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL12'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl12
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL13'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl13
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL14'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl14
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL15'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl15
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL16'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl16
+ ,(SELECT
+      e.Sigla
+    FROM RajaMine.dbo.ElementoXTipoProcesso etp
+    INNER JOIN RajaMine.dbo.Elemento e
+      ON e.id = etp.IdElemento
+    WHERE etp.IdTipoProcesso = pr.IdTipoProcesso
+    AND e.Campo = 'EL17'
+    AND e.Tipo = 1--GranulomÃ©trica
+  )
+  NomeEl17
+FROM RajaMine.dbo.Processo pr
+INNER JOIN RajaMine.dbo.TipoProcesso tp
+  ON tp.Id = pr.IdTipoProcesso
+INNER JOIN RajaMine.dbo.PlantaTurno t
+  ON t.Id = pr.IdPlantaTurno
+)
+SELECT 
+  [COD EMPRESA],
+  [TIPO PROCESSO],
+  [DATA],
+  [TONELADAS],
+  [TEMPO PROCESSAMENTO],
+  [TURNO],
+  [ANÁLISE INFORMADA],
+  [ANÁLISE GRANOLOMÉTRICA INFORMADA],
+  U.VALOR,
+  U.ELEMENTO
+FROM BaseQuery
+CROSS APPLY (
+  VALUES 
+    (EL1, NomeEl1),
+    (EL2, NomeEl2),
+    (EL3, NomeEl3),
+    (EL4, NomeEl4),
+    (EL5, NomeEl5),
+    (EL6, NomeEl6),
+    (EL7, NomeEl7),
+    (EL8, NomeEl8),
+    (EL9, NomeEl9),
+    (EL10, NomeEl10),
+    (EL11, NomeEl11),
+    (EL12, NomeEl12),
+    (EL13, NomeEl13),
+    (EL14, NomeEl14),
+    (EL15, NomeEl15),
+    (EL16, NomeEl16),
+    (EL17, NomeEl17)
+) U(VALOR, ELEMENTO)
+WHERE U.VALOR IS NOT NULL AND U.VALOR <> 0
+GO
